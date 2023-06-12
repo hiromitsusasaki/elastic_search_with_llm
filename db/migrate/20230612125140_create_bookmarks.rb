849cddec8507efd8786782522f7d9939cc7f4688
hiromitsusasaki <hiromitsu.sasaki@gmail.com>
2023-06-12 23:31:28 +0900

    feat: add annotate gem

--- db/migrate/20230612125140_create_bookmarks.rb
+++ db/migrate/20230612125140_create_bookmarks.rb
@@ -1,10 +1,10 @@
 class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
-      t.string :title
+      t.text :title
       t.text :description
-      t.string :url
-      t.integer :indentifier_in_source
+      t.text :url
+      t.integer :identifier_in_source
       t.datetime :created_in_source_at
       t.datetime :updated_in_source_at
 
--- spec/factories/bookmarks.rb
+++ spec/factories/bookmarks.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: bookmarks
+#
+#  id                   :bigint           not null, primary key
+#  created_in_source_at :datetime
+#  description          :text(65535)
+#  identifier_in_source :integer
+#  title                :text(65535)
+#  updated_in_source_at :datetime
+#  url                  :text(65535)
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
 FactoryBot.define do
   factory :bookmark do
     
