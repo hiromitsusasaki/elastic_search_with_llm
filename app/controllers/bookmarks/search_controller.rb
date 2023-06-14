bfa7db16479449a06f314e71d0056bda5aa38684
hiromitsusasaki <hiromitsu.sasaki@gmail.com>
2023-06-14 23:51:43 +0900

    feat: add pagination using kaminari

--- app/controllers/bookmarks/search_controller.rb
+++ app/controllers/bookmarks/search_controller.rb
@@ -1,12 +1,21 @@
 class Bookmarks::SearchController < ApplicationController
 
   def index
-    if params[:query].blank?
-      @bookmarks = []
+    @query = query
+    @bookmarks = if @query.present?
+      Bookmark.es_search(@query).page(page || 1).per(10).records
     else
-      @bookmarks = Bookmark.es_search(params[:query]).records
-      @query = params[:query]
+      Bookmark.page(page || 1).per(10)
     end
   end
 
+  private
+
+  def query
+    params[:query]
+  end
+
+  def page
+    params[:page]
+  end
 end
